fa5855a084a6d2fc631d88b80294aee7f57de5bc
Sepehr Goodarzi <58171264+sepehrgoodarzi6@users.noreply.github.com>
2021-01-08 05:06:01 +0330

    Add files via upload

---
+++ Installation-Script.sh
@@ -0,0 +1,7 @@
+sudo apt install python3
+sudo apt-get install git python3-pip
+git clone https://github.com/threat9/routersploit
+cd routersploit
+python3 -m pip install -r requirements.txt
+sudo apt-get install libglib2.0-dev
+python3 -m pip install bluepy
